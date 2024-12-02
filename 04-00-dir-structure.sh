94b85e10e2e710cbcc12bcb3bacbe24d263b918b
sj <sj@flexsolution.eu>
2024-12-02 13:41:32 +0100

    file structure added

--- 04-00-dir-structure.sh
+++ 04-00-dir-structure.sh
@@ -1,4 +1,20 @@
 # Following directory structure will be created
+mkdir -p /var/system/{tools,scripts,certs,backup}
 
+# Create under directories in srv
+mkdir -p /srv/{tasks,http,smb}
 
-mkdir 
+# Creating specifc smb folders
+mkdir -p /srv/smb/share
+mkdir -p /srv/smb/backup
+
+# Creating tasks specifc folders
+mkdir -p /srv/tasks/CORE-2.0.0-SNAPSHOT
+ln -sf /srv/tasks/CORE-2.0.0/ /srv/tasks/CURRENT
+
+mkdir -p /srv/tasks/CURRENT/{default,libs}
+ln -sf /var/system/certs/ /srv/tasks/CURRENT/default/certs
+ln -sf /srv/tasks/CURRENT/libs/ /srv/tasks/CURRENT/default/libs
+
+echo "File Structure created"
+echo "Next Script will be 04-01-init-tasks.sh"
