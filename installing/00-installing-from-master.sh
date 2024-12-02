ff9c7330cc298f50b2ff35044646a4be2c3b14c2
sj <sj@flexsolution.eu>
2024-12-02 15:35:36 +0100

    bug fix

--- installing/00-installing-from-master.sh
+++ installing/00-installing-from-master.sh
@@ -1,7 +1,9 @@
-if ! `/usr/bin/nc -w 5 "10.1.1.70" 22`; then 
-    echo "Can not reach 10.1.1.70 - do u have a internet connection?"
+IP=10.1.1.69
+
+if ! `/usr/bin/nc -w 5 "$IP" 22`; then 
+    echo "Can not reach "$IP" - do u have a internet connection?"
     ip a
-    ping 10.1.1.70
+    ping -W 5 -c 2 "$IP"
     exit 1
 fi
 
@@ -10,14 +12,14 @@ echo "Please enter a valid disk for write the master on (/dev/sda)"
 read -p "Disk: " DISK
 
 echo "Before writing on ${DSIK} please provide the second partition to chroot into"
-read "the second partiton (/dev/sda2): " PART2
+read -p "the second partiton (/dev/sda2): " PART2
 
 read -p "Have you already installed a the master on this system? (y/n): " INSTALLED
 
 if [[ $INSTALLED == "n" ]]; then
     echo "okay - installing system from master now..."
     echo "Take around 15 minutes"
-    ssh root@10.1.1.70 "dd if=/dev/sda bs=64M | gzip" | pv | gunzip | dd of=$DISK bs=64M
+    ssh root@"$IP" "dd if=/dev/sda bs=64M | gzip" | pv | gunzip | dd of=$DISK bs=64M
 fi
 
 echo "Resizing partiton 2 to maximum"
