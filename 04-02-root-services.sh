851f5e443a632ea2ba5e4e2bf85fee1384e30a7f
sj <sj@flexsolution.eu>
2024-12-02 15:09:30 +0100

    echo

--- 04-02-root-services.sh
+++ 04-02-root-services.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # Enabling Snapper automatic snapshots.
-echo "Enabling Snapper and automatic snapshots entries."
+echo "enabling Snapper and automatic snapshots entries."
 systemctl enable snapper-timeline.timer
 systemctl enable snapper-cleanup.timer
 systemctl enable grub-btrfsd.service
 
 # Enabling systemd-networkd and sshd
+echo "enabling systemd-networkd and sshd"
 systemctl enable systemd-networkd
 systemctl enable sshd
 
 # Enabling services needed for tasks (04-01-init-tasks.sh)
+echo "enabling services needed for tasks (04-01-init-tasks.sh)"
 systemctl enable smb
 systemctl enable mosquitto
 systemctl enable caddy
