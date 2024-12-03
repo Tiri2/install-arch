9653451631ed69adfc74161fc0ef4e4febe72b9a
sj <sj@flexsolution.eu>
2024-12-03 12:20:06 +0100

    d

--- installing/01-deleting-snapshots.sh
+++ installing/01-deleting-snapshots.sh
@@ -30,13 +30,17 @@ fi
 last_snapshot_id=$(echo "$snapshot_ids" | tail -n 1)
 # Wichtigste Snapshot-ID definieren
 important_snapshot_id="1"
+# IDs, die nicht gelöscht werden sollen
+KEEP_IDS=("256" "257" "258" "259" "260" "263" "265" "266" "268", "269", "270", "272") 
 
 echo "DEBUG: last_snapshot_id: ${last_snapshot_id}"
 echo "DEBUG: snapshots_id: ${snapshot_ids}"
 
 # Durchlaufen und Snapshots löschen, außer den letzten und wichtigsten
 for snapshot_id in $snapshot_ids; do
-  if [[ "$snapshot_id" == "$last_snapshot_id" || "$snapshot_id" == "$important_snapshot_id" ]]; then
+    if [[ " ${KEEP_IDS[@]} " =~ " $snapshot_id " ]]; then
+    echo "Behalte Subvolume mit ID weil in keep_ids: $snapshot_id"
+  elif [[ "$snapshot_id" == "$last_snapshot_id" || "$snapshot_id" == "$important_snapshot_id" ]]; then
     echo "Behalte Snapshot mit ID: $snapshot_id"
   else
     echo "Lösche Snapshot mit ID: $snapshot_id"
