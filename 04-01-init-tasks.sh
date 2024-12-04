89d5e0e83f5176482c80f0042da0d126e8ce4640
sj <sj@flexsolution.eu>
2024-12-04 10:47:52 +0100

    bug fix

--- 04-01-init-tasks.sh
+++ 04-01-init-tasks.sh
@@ -57,7 +57,7 @@ cat configs/samba.txt > /etc/samba/smb.conf
 
 echo "installing and setting up caddy server"
 pacman -S --noconfirm caddy
-cat configs/caddyfile.txt > /etc/caddy/CaddyFile
+cat configs/caddyfile.txt > /etc/caddy/Caddyfile
 
 echo "Setting up systemd-networkd and resolve.conf"
 cat configs/systemd-network.txt > /etc/systemd/network/10-ethernet.network
