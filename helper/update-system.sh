0f41426abebc7c90ee28a61ed780bf2d315277e9
sj <sj@flexsolution.eu>
2024-12-11 15:59:17 +0100

    easy updating script added

---
+++ helper/update-system.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "[$(date)] Starting updating system" >> /var/flex/updates.log
+start_time=$(date +%s)
+
+pacman -Syu
+
+if [[ -d "/var/system/tools/install-arch/" ]]; then
+    git -C /var/system/tools/install-arch/ pull
+else
+    git clone https://github.com/Tiri2/install-arch.git /var/system/tools/install-arch/
+fi
+
+
+cd /var/system/tools/install-arch
+cp misc/.zshrc /root
+
+cp misc/.zshrc /home/flex
+chown flex:flex /home/flex/.zshrc
+
+end_time=$(date +%s)
+duration=$((end_time - start_time))
+
+echo "[$(date)] Successfully updated system" >> /var/flex/updates.log
+echo "[$(date)] Took ${duration}s to updating" >> /var/flex/updates.log
