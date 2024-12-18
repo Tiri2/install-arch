2c15a4a41e0023d998ffc20eeea1ce3e1103ca6a
sj <sj@flexsolution.eu>
2024-12-18 14:51:30 +0100

    d

--- 01-partitioning.sh
+++ 01-partitioning.sh
@@ -11,15 +11,15 @@ if [ -z "$PART" ]; then
     read -r -p "Please choose the partition name: " PART
 fi
 
-if [ -z "$BOOT_PART" ]; then
-    read -r -p "Please choose the boot partition: " PART
-fi
-
 parted "$PART" -- mklabel gpt
 
 parted "$PART" -- mkpart ESP fat32 1MiB 512MiB
 parted "$PART" -- mkpart primary 512MiB 100%
 
 lsblk
+
+if [ -z "$BOOT_PART" ]; then
+    read -r -p "Please choose the boot partition: " PART
+fi
 
 mkfs.vfat "${BOOT_PART}"
