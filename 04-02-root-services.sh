36ca469e92ab94cbb7e19697aea9089910502116
sj <sj@flexsolution.eu>
2024-12-23 11:30:24 +0100

    configurator added

--- 04-02-root-services.sh
+++ 04-02-root-services.sh
@@ -16,3 +16,13 @@ echo "enabling services needed for tasks (04-01-init-tasks.sh)"
 systemctl enable smb
 systemctl enable mosquitto
 systemctl enable caddy
+
+# Setting up configurator
+echo "Setting up configurator"
+mkdir -p /var/system/tools/configurator/
+cat configs/configurator/log4j2.xml.txt > /var/system/tools/configurator/log4j2.xml
+cat configs/configurator/configurator.path.txt > /etc/systemd/system/system.configurator.path
+cat configs/configurator/configurator.service.txt > /etc/systemd/system/system.configurator.service
+cp configs/configurator/linux-configurator-1.0.0-SNAPSHOT.jar /var/system/tools/configurator
+ln -sf /var/system/tools/configurator/linux-configurator-1.0.0-SNAPSHOT.jar configurator.jar
+systemctl enable system.configurator.path
