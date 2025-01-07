0e318f76f6651fa1281c421f4536b6f9dedf3c13
sj <sj@flexsolution.eu>
2025-01-07 16:22:43 +0100

    d

--- backup/restore/00-installing-from-backup.sh
+++ backup/restore/00-installing-from-backup.sh
@@ -19,13 +19,65 @@ SUBVOLS=(
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 cd $SCRIPT_DIR
 
-# Format the hard disk
-echo "executing 01-partitioning.sh"
-source ../../01-partitioning.sh
+function formatted() {
+  if [[ ! -f "$HOME/.formatted" ]]; then
+    return 1  # Nicht formatiert
+  else
+    return 0  # Bereits formatiert
+  fi
+}
 
-echo "executing 02-1-format.sh"
-source ../../02-1-format.sh
+# Format the hard disk
+if ! formatted; then
+  echo "executing 01-partitioning.sh"
+  source ../01-partitioning.sh
+
+  echo "executing 02-1-format.sh"
+  source ../02-1-format.sh
+
+  # Erstelle die Datei als Marker, dass die Festplatte formatiert wurde
+  touch "$HOME/.formatted"
+else
+  echo "Disk already formatted, skipping..."
+fi
 
 echo "BTRFS -> $BTRFS"
+
+BACKUP_DIR="/tmp/backup"
+
+echo "Please enter the full file path of the tar.gz backup file"
+read -p "file (/root/backup-Di-250107.tar.gz): " BACKUP_FILE
+
+mkdir -p "$BACKUP_DIR"
+
+tar -xzf "$BACKUP_FILE" -C "$BACKUP_DIR/raw"
+
+if [ $? -ne 0 ]; then
+  echo "Fehler beim Entpacken von $BACKUP_FILE."
+  exit 1
+fi
+
+echo "Archiv erfolgreich entpackt nach $BACKUP_DIR/raw."
+
+# Alle Dateien mit der Endung .zst finden
+ZST_FILES=($(find "$BACKUP_DIR/raw" -type f -name "*.zst"))
+
+# Prüfen, ob Dateien gefunden wurden
+if [ ${#ZST_FILES[@]} -eq 0 ]; then
+  echo "Keine .zst-Dateien gefunden."
+  exit 1
+fi
+
+echo "Gefundene .zst-Dateien:"
+for file in "${ZST_FILES[@]}"; do
+  echo "$file"
+done
+
+# Jetzt kannst du die Variable ZST_FILES iterieren oder für weitere Verarbeitung nutzen
+# Beispiel: Iteration über alle Dateien
+for zst_file in "${ZST_FILES[@]}"; do
+  echo "Verarbeite $zst_file..."
+  # Hier kannst du etwas mit jeder Datei machen, z.B. entpacken
+done
 
 # TODO den tar datei angeben und diese dann extrahieren und die einzelnen subvolumes dann via btrfs receive einspielen
