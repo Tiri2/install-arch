14331ccac73561508959404470fd8a25eab2249c
sj <sj@pi-tec.at>
2025-07-24 08:10:29 +0200

    add path /srv/http

--- configs/system/backup/backup-flexsystem.sh
+++ configs/system/backup/backup-flexsystem.sh
@@ -6,10 +6,9 @@ roption=(
         --xattrs
         --absolute-names
         --use-compress-program=zstd
-        --ignore-failed-read        # fehlende Pfade überspringen
+        --ignore-failed-read
 )
 
-# Nur die reinen Config-Dateien, keine Datenverzeichnisse
 configs=(
   /etc/mosquitto/
   /etc/caddy
@@ -18,11 +17,8 @@ configs=(
   /etc/grafana.ini
   /etc/systemd/network/*.network
   /home/flex/.config/*
+  /srv/http/
 )
 
-# Backup starten:
-# 1) Einträge aus backup.include
-# 2) alle Config-Pfade (wird übersprungen, wenn’s sie nicht gibt)
-# 3) alle .db in /srv/tasks/CURRENT nach foption
 tar "${roption[@]}" \
     "${configs[@]}" 
